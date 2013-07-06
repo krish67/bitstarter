17de847d30b20b120cbd962413bdd06a3b89454a
Krishnan <rkrish67@gmail.com>
2013-07-06 14:22:07 +0000

    Read index from file

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  // response.send('Hello World 2!');
+  var data = fs.readFileSync('index.html');
+  var result = data.toString();
+  response.send(result);
 });
 
 var port = process.env.PORT || 5000;
